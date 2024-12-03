e837f41bb00992bd173b8bc6bc79b52e58c5be2d
Dirk Mahler <dirk.mahler@buschmais.com>
2024-12-03 16:58:02 +0100

    added liquibase

--- service/src/main/java/org/jqassistant/tooling/dashboard/service/adapters/secondary/xo/configuration/XOConfiguration.java
+++ service/src/main/java/org/jqassistant/tooling/dashboard/service/adapters/secondary/xo/configuration/XOConfiguration.java
@@ -2,6 +2,8 @@ package org.jqassistant.tooling.dashboard.service.adapters.secondary.xo.configur
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
 
@@ -13,6 +15,13 @@ import com.buschmais.xo.spring.XOAutoConfiguration;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import liquibase.Scope;
+import liquibase.command.CommandScope;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.ext.neo4j.database.jdbc.Neo4jDriver;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.RequiredArgsConstructor;
 import org.jqassistant.tooling.dashboard.service.adapters.secondary.xo.*;
 import org.jqassistant.tooling.dashboard.service.application.model.*;
@@ -31,6 +40,7 @@ public class XOConfiguration {
 
     @PostConstruct
     void init() throws URISyntaxException {
+        migrate();
         Properties properties = new Properties();
         properties.setProperty(RemoteNeo4jXOProvider.Property.USERNAME.getKey(), xoConfigurationProperties.getUsername());
         properties.setProperty(RemoteNeo4jXOProvider.Property.PASSWORD.getKey(), xoConfigurationProperties.getPassword());
@@ -57,5 +67,29 @@ public class XOConfiguration {
     @Bean
     public XOManagerFactory getXOManagerFactory() {
         return xoManagerFactory;
+    }
+
+    private void migrate() {
+        try {
+            Connection connection = openConnection(); //your openConnection logic
+            Database database = DatabaseFactory.getInstance()
+                .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Scope.child(Scope.Attr.resourceAccessor, new ClassLoaderResourceAccessor(), () -> {
+                CommandScope update = new CommandScope("update");
+                update.addArgumentValue("changelogFile", "db/changelog/changelog.xml");
+                update.addArgumentValue("database", database);
+
+                update.execute();
+            });
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private Connection openConnection() throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("user", "neo4j");
+        properties.setProperty("password", "<redacted>");
+        return new Neo4jDriver().connect("jdbc:neo4j:" + xoConfigurationProperties.getUrl(), properties);
     }
 }
