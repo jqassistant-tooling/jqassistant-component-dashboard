1a7c97c1c0e2ab694ddc734bcaf2ddd9b913f482
Dirk Mahler <dirk.mahler@buschmais.com>
2025-01-03 13:53:55 +0100

    enabled VersionControllerTest

--- service/src/test/java/org/jqassistant/tooling/dashboard/service/adapters/primary/api/rest/VersionControllerTest.java
+++ service/src/test/java/org/jqassistant/tooling/dashboard/service/adapters/primary/api/rest/VersionControllerTest.java
@@ -1,32 +1,31 @@
 package org.jqassistant.tooling.dashboard.service.adapters.primary.api.rest;
 
-import org.jqassistant.tooling.dashboard.service.adapters.primary.api.rest.auth.AuthenticationFilter;
-import org.jqassistant.tooling.dashboard.service.adapters.primary.api.rest.config.RestApiProperties;
+import org.jqassistant.tooling.dashboard.api.dto.VersionDTO;
 import org.jqassistant.tooling.dashboard.service.adapters.primary.api.rest.mapper.VersionMapper;
 import org.jqassistant.tooling.dashboard.service.application.ComponentService;
 import org.jqassistant.tooling.dashboard.service.application.ProjectService;
 import org.jqassistant.tooling.dashboard.service.application.VersionService;
-import org.junit.jupiter.api.Disabled;
+import org.jqassistant.tooling.dashboard.service.application.model.Component;
+import org.jqassistant.tooling.dashboard.service.application.model.Project;
+import org.jqassistant.tooling.dashboard.service.application.model.ProjectKey;
+import org.jqassistant.tooling.dashboard.service.application.model.Version;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
 @WebMvcTest(VersionController.class)
 class VersionControllerTest {
-
-    @MockBean
-    private RestApiProperties restApiProperties;
 
     @MockBean
     private ProjectService projectService;
@@ -43,17 +42,45 @@ class VersionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Disabled
     @Test
-    void getVersion() throws Exception {
-        String apiKey = "secret_token";
-        Mockito.doReturn(apiKey)
-            .when(restApiProperties)
-            .getAuthToken();
-        mockMvc.perform(put("/api/rest/v1/jqassistant/plugins/test/1.0.0").header(AuthenticationFilter.AUTH_TOKEN_HEADER_NAME, apiKey)
+    void updateVersion() throws Exception {
+        Project project = project("plugins");
+        Component component = component("test");
+
+        ProjectKey projectKey = new ProjectKey("jqassistant", "plugins");
+        doReturn(project).when(projectService)
+            .find(projectKey);
+        doReturn(component).when(componentService)
+            .resolve(projectKey, "test");
+        Version version = mock(Version.class);
+        doReturn(version).when(versionMapper)
+            .toVersion(eq(project), eq(component), any(VersionDTO.class));
+
+        mockMvc.perform(put("/api/rest/v1/jqassistant/plugins/test/1.0.0").content("{ }")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())
+                .with(user("user").roles("USER"))
                 .accept(MediaType.APPLICATION_JSON))
-            .andDo(print())
             .andExpect(status().isOk());
+
+        verify(projectService).find(projectKey);
+        verify(componentService).resolve(projectKey, "test");
+        verify(versionService).remove(component, "1.0.0");
+        verify(versionMapper).toVersion(eq(project), eq(component), any(VersionDTO.class));
+    }
+
+    private static Component component(String name) {
+        Component component = mock(Component.class);
+        doReturn(name).when(component)
+            .getName();
+        return component;
+    }
+
+    private static Project project(String name) {
+        Project project = mock(Project.class);
+        doReturn(name).when(project)
+            .getName();
+        return project;
     }
 
 }
