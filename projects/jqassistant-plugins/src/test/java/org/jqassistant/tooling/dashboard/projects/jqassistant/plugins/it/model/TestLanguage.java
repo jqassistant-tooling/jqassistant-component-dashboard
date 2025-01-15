f9905ab3b029e95ee6ce2d2033343538e90d84f0
Dirk Mahler <dirk.mahler@buschmais.com>
2025-01-15 18:39:41 +0100

    fixed failing IT

--- projects/jqassistant-plugins/src/test/java/org/jqassistant/tooling/dashboard/projects/jqassistant/plugins/it/model/TestLanguage.java
+++ projects/jqassistant-plugins/src/test/java/org/jqassistant/tooling/dashboard/projects/jqassistant/plugins/it/model/TestLanguage.java
@@ -1,4 +1,4 @@
-package org.jqassistant.tooling.dashboard.plugin.it.model;
+package org.jqassistant.tooling.dashboard.projects.jqassistant.plugins.it.model;
 
 import com.buschmais.xo.api.annotation.Abstract;
 import com.buschmais.xo.neo4j.api.annotation.Label;
