491f3ad97a167ab32fcc25555188063f186afa49
Tobias Krakau <tobias.krakau.berlin@gmail.com>
2025-06-13 16:02:06 +0200

    #64 added ContributorRepository and ContributorService

---
+++ plugin/src/main/java/org/jqassistant/tooling/dashboard/plugin/impl/mapper/ContributorMapper.java
@@ -0,0 +1,18 @@
+package org.jqassistant.tooling.dashboard.plugin.impl.mapper;
+
+import org.jqassistant.tooling.dashboard.api.dto.VersionDTO;
+import org.jqassistant.tooling.dashboard.plugin.api.model.Contributor;
+import org.jqassistant.tooling.dashboard.plugin.api.model.Version;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = FileMapper.class)
+public interface ContributorMapper {
+
+    ContributorMapper MAPPER = Mappers.getMapper(ContributorMapper.class);
+
+    @BeanMapping(ignoreUnmappedSourceProperties = { "id", "delegate", "component" })
+    ContributorDTO toDTO(Contributor contributor);
+
+}
