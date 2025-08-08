4c4a67151eb89599b427fd31122af321fbbb52ca
Tobias Krakau <tobias.krakau.berlin@gmail.com>
2025-08-08 17:20:40 +0200

    Merge remote-tracking branch 'origin/111-extend-persistent-model-by-contribution' into 111-extend-persistent-model-by-contribution

--- service/src/main/java/org/jqassistant/tooling/dashboard/service/application/model/Contributions.java
+++ service/src/main/java/org/jqassistant/tooling/dashboard/service/application/model/Contributions.java
@@ -2,7 +2,6 @@ package org.jqassistant.tooling.dashboard.service.application.model;
 
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
-import com.buschmais.xo.neo4j.api.annotation.Relation.Incoming;
 import com.buschmais.xo.neo4j.api.annotation.Relation.Outgoing;
 
 @Label("Contributions")
@@ -11,19 +10,8 @@ public interface Contributions {
     int getCommits();
     void setCommits(int commits);
 
-    @ToComponent
+    @Relation("TO_COMPONENT")
     @Outgoing
     Component getToComponent();
     void setToComponent(Component component);
-
-    @FromContributor
-    @Incoming
-    Contributor getContributor();
-    void setContributor(Contributor contributor);
-
-    @Relation("TO_COMPONENT")
-    @interface ToComponent {}
-
-    @Relation("CONTRIBUTED")
-    @interface FromContributor {}
 }
